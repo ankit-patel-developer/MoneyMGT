945732a4ca4a43d59b1837a93013549764bcf29a
ankit-patel-developer <ankitjpatel2007@gmail.com>
2025-02-26 18:21:22 -0600

    bank-accounts-wip1

---
+++ MoneyMGTAPI/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.DTOs;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyMGTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private APIResponse _response;
+        private readonly IAccountRepository _acRepo;
+
+        public AccountController(IAccountRepository acRepo)
+        {
+            _acRepo = acRepo;
+        }
+
+        // ok
+        [HttpGet]
+        [Route("getBankAccounts/{bankId}")]
+        public IActionResult GetBankAccounts(int bankId)
+        {
+            var bankAccounts = _acRepo.GetBankAccounts(bankId);
+            return Ok(bankAccounts);
+        }
+    }
+}
--- Services/Repositories/AccountRepository.cs
+++ Services/Repositories/AccountRepository.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataLayer;
 using DataLayer.Models;
-using Services.DTOs;
 using Services.Interfaces;
+using System.Linq;
+using Services.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace Services.Repositories
 {
     public class AccountRepository : IAccountRepository
     {
+        private readonly MoneyMGTContext appDbContext;
+
+        public AccountRepository(MoneyMGTContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public IEnumerable<AccountListVM> GetAllAccounts()
+        {
+            List<AccountListVM> accounts = new List<AccountListVM>();
+            var accountsDb = appDbContext.Accounts.Include(x => x.Bank);
+            if (accountsDb != null)
+            {
+                foreach (var ac in accountsDb)
+                {
+                    accounts.Add(new AccountListVM()
+                    {
+                        AccountId = ac.AccountId,
+                        AccountType = ac.AccountType,
+                        AccountNumber = ac.AccountNumber,
+                        Balance = ac.Balance,
+                        BankId = ac.BankId,
+                        BankName = ac.Bank.BankName
+                    });
+                }
+            }
+            return accounts;
+        }
+
         public Account AddAccount(Account account)
         {
-            throw new NotImplementedException();
+            var result = appDbContext.Accounts.Add(account);
+            appDbContext.SaveChanges();
+            return result.Entity;
         }
 
         public Account EditAccount(Account account)
@@ -22,21 +56,50 @@ namespace Services.Repositories
         public Account GetAccount(int accountId)
         {
             throw new NotImplementedException();
-        }
-
-        public IEnumerable<AccountListVM> GetAllAccounts()
-        {
-            throw new NotImplementedException();
-        }
+        }    
 
         public List<string> GetAllAccountTypes()
         {
             throw new NotImplementedException();
         }
 
-        public BankAccountVM GetBankAccounts(int bankId)
+        IEnumerable<AccountListVM> IAccountRepository.GetAllAccounts()
         {
             throw new NotImplementedException();
+        }
+
+        public BankAccountVM GetBankAccounts(int bankId)
+        {
+            BankAccountVM bankAccountVM = new BankAccountVM();
+            List<AccountVM> accounts = new List<AccountVM>();
+            bankAccountVM.Accounts = accounts;
+            bankAccountVM.BankId = bankId;
+            var bankDb = appDbContext.Banks.Where(x => x.BankId == bankId).FirstOrDefault();
+
+            if (bankDb != null)
+            {
+                bankAccountVM.BankName = bankDb.BankName;
+
+                var accountsDb = appDbContext.Accounts.Where(x => x.BankId == bankId).Include(y => y.Bank);
+                if (accountsDb != null)
+                {
+                    foreach (var ac in accountsDb)
+                    {
+                        accounts.Add(new AccountVM()
+                        {
+                            AccountId = ac.AccountId,
+                            AccountType = ac.AccountType,
+                            AccountNumber = ac.AccountNumber,
+                            Balance = ac.Balance
+                        });
+                    }
+                }
+                return bankAccountVM;
+            }
+            else
+            {
+                return bankAccountVM;
+            }
         }
     }
 }
