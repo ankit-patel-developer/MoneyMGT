bdaedf9edc38d700b73ccabbe37f7658326edfd3
ankit-patel-developer <ankitjpatel2007@gmail.com>
2025-08-26 19:07:54 -0500

    vr-wip7

--- Services/Repositories/VirtualTransactionsRepository.cs
+++ Services/Repositories/VirtualTransactionsRepository.cs
@@ -176,8 +176,154 @@ source id = 2
         // Withdraw Transaction from Bank
         public bool WithdrawToPayee()
         {
-        
-            return true;
+			/*
+CREATE PROCEDURE VT_WITHDRAW 
+      @BankId int, 
+	  @AccountNumber int	   
+AS 
+BEGIN
+ 
+    SET NOCOUNT ON; 
+	
+	declare @currentBalance_ decimal(18,2);
+	declare @minusBalance decimal(18,2);
+	declare @updatedBalance decimal(18,2);
+
+	declare @AccountId int;
+	
+	declare @PayeeId int;
+	declare @TransactionAmount decimal(18,2);
+	declare @TransactionDate datetime; -- current datetime
+	declare @TransactionStatus int;
+	declare @LastBalance decimal(18,2);
+	declare @CurrentBalance decimal(18,2);
+	declare @RefCode varchar(6);
+	declare @TransactionType int;
+	declare @SourceId int;
+	
+	
+
+	BEGIN TRY
+
+		-- 10 TRANSACTIONS 
+		declare @cnt int = 0;
+		WHILE @cnt<10
+		BEGIN
+			-- find current account balance
+			select @currentBalance_ = Balance, @AccountId = AccountId
+			from Accounts
+			where BankId=@BankId and AccountNumber=@AccountNumber;
+			print 'current balance for a/c no = ' + cast(@AccountNumber as varchar(20)) + ' = $' + cast(@currentBalance_ as varchar(20));
+
+			-- random generate amount to be deposit
+			-- random decimal between 0 and 1000, rounded to 2 decimal places
+			select @minusBalance = ROUND(RAND() * 1000, 2);
+			select @updatedBalance = @currentBalance_ - @minusBalance;
+			print 'minus balance = ' + cast(@minusBalance as varchar(20))
+			print 'updated balance for a/c no = ' + cast(@AccountNumber as varchar(20)) + ' = $' + cast(@updatedBalance as varchar(20));
+
+			-- CHECK IF UPDATED BALANCE IS < 0 THEN 
+			-- CONTINUE WITH NEXT VALUE OF LOOP
+			if(@updatedBalance < 0)
+				CONTINUE;
+
+			-- - bank
+			-- PayeeId = 0
+			select @SourceId = 0;
+			select @TransactionAmount = @minusBalance;
+			select @TransactionDate = CURRENT_TIMESTAMP;
+			select @TransactionStatus = 0; -- success
+			select @LastBalance = @currentBalance_;
+			select @CurrentBalance = @updatedBalance;
+			-- SELECT SUBSTRING(CONVERT(varchar(255), NEWID()), 0, 7);
+			select @RefCode = SUBSTRING(CONVERT(varchar(255), NEWID()), 0, 7);
+			select @TransactionType = 1; -- OUT
+
+			-- random generate PayeeId (1-5)
+			-- random integer between 1 and 5 (inclusive)
+			select @PayeeId = FLOOR(RAND() * 5) + 1;
+
+			print 'payee id = ' + cast(@PayeeId as varchar(20));
+			print 'transaction amount = ' + cast(@TransactionAmount as varchar(20));
+			print 'transaction date = ' + cast(@TransactionDate as varchar(20));
+			print 'transaction status = ' + cast(@TransactionStatus as varchar(20));
+			print 'bank id = ' + cast(@BankId as varchar(20));
+			print 'account id = ' + cast(@AccountId as varchar(20));
+			print 'last balance = ' + cast(@LastBalance as varchar(20));
+			print 'current balance = ' + cast(@CurrentBalance as varchar(20));
+			print 'ref code = ' + cast(@RefCode as varchar(6));
+			print 'transaction type = ' + cast(@TransactionType as varchar(20));
+			print 'source id = ' + cast(@SourceId as varchar(20));
+
+
+			-- sql transaction - try/catch
+			BEGIN TRY
+				BEGIN TRANSACTION;
+			
+					-- update Balance column @ Accounts table
+					update Accounts
+					set Balance = @CurrentBalance
+					where AccountId=@AccountId and AccountNumber = @AccountNumber;
+
+					-- insert @ BankTransactions table
+					insert into BankTransactions
+					(PayeeId, TransactionAmount, TransactionDate, TransactionStatus, BankId, AccountId, LastBalance, CurrentBalance, RefCode, TransactionType,SourceId )
+					values
+					(@PayeeId, @TransactionAmount, @TransactionDate, @TransactionStatus, @BankId, @AccountId, @LastBalance, @CurrentBalance, @RefCode, @TransactionType, @SourceId);
+				
+					-- check for exception
+					-- (@PayeeId, 'xxx', @TransactionDate, @TransactionStatus, @BankId, @AccountId, @LastBalance, @CurrentBalance, @RefCode, @TransactionType, @SourceId);
+			
+
+					
+					
+					declare @payeeType int;
+					declare @payeeBalance decimal(18,2);
+					select @payeeType = PayeeType, @payeeBalance = Balance
+					from Payees
+					where PayeeId=@PayeeId;
+					
+					-- check if payee type (PayeeType=3) is creditcard then
+					-- add amount @ cc balance
+					if @payeeType=3 
+					begin
+						select @payeeBalance = @payeeBalance + @TransactionAmount;
+					end
+
+					-- add transaction @ CreditCardTransactions db table
+					-- with same RefCode generated @ BankTransactions db table
+					-- with transaction type In
+						
+					
+				COMMIT TRANSACTION;
+				SET @cnt = @cnt + 1;
+			END TRY
+			BEGIN CATCH
+				IF @@TRANCOUNT > 0
+				BEGIN
+					ROLLBACK TRANSACTION;
+				END
+				DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+				DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+				DECLARE @ErrorState INT = ERROR_STATE();
+				PRINT 'An error occurred: ' + ERROR_MESSAGE();
+				-- RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+				RETURN; -- Exit the stored procedure
+
+				
+			END CATCH
+		END
+		-- END OF 10 TRANSACTIONS
+	END TRY
+	BEGIN CATCH
+
+	END CATCH
+	
+END
+GO   
+			*/
+
+			return true;
         }  
 
     }
